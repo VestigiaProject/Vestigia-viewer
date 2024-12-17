c15ac11f8096491e04db1e960e975031c8da299b
raf780 <150580218+VestigiaProject@users.noreply.github.com>
2024-12-17 15:00:26 +0100

    Added user profiles

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -17,7 +17,7 @@ export default function AuthPage() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/timeline`,
+          redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
       if (error) throw error;
